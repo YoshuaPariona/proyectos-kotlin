3ce5938e5454a375af35ff4164972eb90096029b
Yoshua <loganpariona.1207@gmail.com>
2025-03-29 18:46:58 -0500

    Crear navegación de pantallas

--- van-02-calculadora/app/src/main/java/com/example/calculadora/CalculadoraActivity.kt
+++ van-02-calculadora/app/src/main/java/com/example/calculadora/CalculadoraActivity.kt
@@ -1,4 +1,37 @@
 package com.example.calculadora
 
-class CalculadoraActivity {
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.TextView
+import androidx.activity.ComponentActivity
+import com.example.calculadora.databinding.ActivityCalculadoraBinding
+
+class CalculadoraActivity : ComponentActivity() {
+
+    private lateinit var binding: ActivityCalculadoraBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        binding = ActivityCalculadoraBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val mensaje = intent.getStringExtra("mensaje") ?: "Bienvenido a la calculadora"
+
+        var num1 = Integer.parseInt(binding.etNum1.text.toString())
+        var num2 = Integer.parseInt(binding.etNum2.text.toString())
+
+        binding.tvMensaje.text = mensaje
+
+        binding.btnBack.setOnClickListener{
+            finish()
+        }
+
+        binding.calcSum.setOnClickListener{
+            var resultado = num1 + num2
+        }
+    }
+
 }
--- van-02-calculadora/app/src/main/java/com/example/calculadora/MainActivity.kt
+++ van-02-calculadora/app/src/main/java/com/example/calculadora/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.example.calculadora
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.activity.ComponentActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+    fun abrirCalc(view:View) {
+        val intent = Intent(this, CalculadoraActivity::class.java)
+        intent.putExtra("mensaje", "Iniciando al calculadora correctamente")
+        startActivity(intent)
+    }
+    fun shareApp(view: View) {
+        val intent = Intent(Intent.ACTION_SEND)
+        intent.type = "text/plain"
+        intent.putExtra(Intent.EXTRA_TEXT, "Prueba mi app de calculadora !!!")
+        startActivity(Intent.createChooser(intent,"Compartir con . . ."))
     }
 }
